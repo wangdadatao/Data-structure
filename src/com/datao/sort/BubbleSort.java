87adda37c1272b5a6883151c71e6269b62611de3
wanghaitao <358981721@qq.com>
2018-11-20 16:58:08 +0800

    添加鸡尾酒排序

--- src/com/datao/sort/BubbleSort.java
+++ src/com/datao/sort/BubbleSort.java
@@ -8,16 +8,19 @@ package com.datao.sort;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] nums = {39, 10, 218,  84, 392, 384, 573, 25, 431,1, 99, 53, 42};
+        int[] nums = {39, 10, 218, 84, 392, 384, 573, 25, 431, 1, 99, 53, 42};
+        sort(nums);
+    }
+
+    private static void sort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - 1 -i; j++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
                 if (nums[j] > nums[j + 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
                 }
             }
-            System.out.println(nums.length);
         }
 
         for (int i : nums) {
---
+++ src/com/datao/sort/CocktailSort.java
@@ -0,0 +1,47 @@
+package com.datao.sort;
+
+/**
+ * 鸡尾酒排序
+ *
+ * @author datao
+ */
+public class CocktailSort {
+
+    public static void main(String[] args) {
+        sort(new int[]{1, 2, 3});
+    }
+
+    private static void sort(int[] nums) {
+        nums = new int[]{39, 10, 218, 84, 392, 384, 573, 25, 431, 1, 99, 53, 42};
+
+        Integer left = 0;
+        Integer right = nums.length - 1;
+
+        while (left < right) {
+            for (int i = left; i < right; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    Integer temp = nums[i + 1];
+                    nums[i + 1] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+            right--;
+
+            for (int i = right; i >= left + 1; i--) {
+                if (nums[i] < nums[i - 1]) {
+                    Integer temp = nums[i - 1];
+                    nums[i - 1] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+            left++;
+            System.out.println(-999);
+        }
+
+        for (Integer i : nums) {
+            System.out.println(i);
+        }
+
+    }
+
+}
